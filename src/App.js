4240c98a4aa790e301df8f905c0263452c49937b
Sreedharan Namboodiri C <89787284+SridharDigital@users.noreply.github.com>
2023-07-28 03:11:53 +0530

    first Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import LoginForm from "./components/LoginForm"
+import "@fontsource/roboto"
+
+import "./App.css"
+
+// Replace your code here
+const App = () => (
+  <div>
+    <LoginForm />
+  </div>
+)
+
+export default App
